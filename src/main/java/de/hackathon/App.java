651e5c86e4b9984fc6e2f9de13101010e55636f7
Ann-Katrin Arning <ann-katrin.arning@web.de>
2014-06-07 17:02:18 +0200

    neue get Methode für ids

--- src/main/java/de/hackathon/App.java
+++ src/main/java/de/hackathon/App.java
@@ -2,13 +2,17 @@ package de.hackathon;
 
 import java.net.UnknownHostException;
 
+import org.bson.types.ObjectId;
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
@@ -26,6 +30,23 @@ public class App {
 			@Override
 			public Object handle(final Request request, final Response response) {
 				final DBObject result = stuffCollection.findOne();
+				return result;
+			}
+		});
+		
+		Spark.get(new Route("/stuff/:id") {
+			@Override
+			public Object handle(final Request request, final Response response) {
+				final String jsId = request.params(":id");
+				DBObject whereQuery = null;
+				if (jsId != null && !jsId.equals(""))
+				{
+					whereQuery = new BasicDBObject();
+					final ObjectId jId = new ObjectId(jsId);
+					whereQuery.put("_id", jId);
+				}
+				DBCursor cursor = stuffCollection.find(whereQuery);
+				final DBObject result = cursor.next();
 				return result;
 			}
 		});
