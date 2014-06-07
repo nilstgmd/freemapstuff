9c286e643504c9aceb59505173cbfb23391a791f
Marvin Hoffmann <spam@marvin-hoffmann.de>
2014-06-07 16:19:31 +0200

    - initial mongo setup

--- src/main/java/de/hackathon/App.java
+++ src/main/java/de/hackathon/App.java
@@ -1,25 +1,41 @@
 package de.hackathon;
 
+import java.net.UnknownHostException;
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	Spark.get(new Route("/") {
-			
+public class App {
+	public static void main(final String[] args) throws UnknownHostException {
+		final MongoClient mongo = new MongoClient();
+		final DB mongoDb = mongo.getDB("freestuffmap");
+		final DBCollection stuffCollection = mongoDb.getCollection("stuff");
+
+		Spark.get(new Route("/stuff/") {
 			@Override
-			public Object handle(Request arg0, Response arg1) {
-				return "Hello World!";
+			public Object handle(final Request request, final Response response) {
+				final DBObject result = stuffCollection.findOne();
+				return result;
 			}
 		});
 
-    }
+		Spark.post(new Route("/stuff/") {
+			@Override
+			public Object handle(final Request request, final Response response) {
+
+				return "Hello World!";
+			}
+		});
+	}
 }
